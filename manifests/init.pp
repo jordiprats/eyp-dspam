b8fa9d8aab6639a6b199521e0a29e6aa1e42a1ec
Jordi Prats <jprats@systemadmin.es>
2016-12-11 14:32:29 +0100

    backends dspam

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,12 @@ class dspam(
                             $manage_docker_service = true,
                             $service_ensure        = 'running',
                             $service_enable        = true,
+                            $backend               = 'mysql',
                           ) inherits dspam::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
+
+  validate_re($backend, [ '^hash$', '^mysql$', '^pgsql$', '^sqlite3$' ], "${backend} is not a supported backend: hash/mysql/pgsql/sqlite3")
 
   class { '::dspam::install': } ->
   class { '::dspam::config': } ~>
--- manifests/params.pp
+++ manifests/params.pp
@@ -12,6 +12,27 @@ class dspam::params {
         /^[5-7].*$/:
         {
           $include_epel=true
+          # [root@centos7 ~]# yum search dspam
+          # Loaded plugins: fastestmirror
+          # Loading mirror speeds from cached hostfile
+          #  * base: centos.cadt.com
+          #  * epel: mirror.airenetworks.es
+          #  * extras: centos.cadt.com
+          #  * updates: mirror.airenetworks.es
+          # ================================================================ N/S matched: dspam =================================================================
+          # dspam-client.x86_64 : The DSPAM client program used to talk to dspam daemon
+          # dspam-devel.x86_64 : Development library and header files for libdspam
+          # dspam-hash.x86_64 : Hash storage driver for libdspam
+          # dspam-libs.x86_64 : The DSPAM library core processing engines
+          # dspam-mysql.x86_64 : MySQL storage driver for libdspam
+          # dspam-pgsql.x86_64 : PostgreSQL storage driver for libdspam
+          # dspam-sqlite3.x86_64 : SQLite v3.x storage driver for libdspam
+          # dspam-web.x86_64 : Web-based interface for DSPAM
+          # dspam.x86_64 : A library and Mail Delivery Agent for Bayesian SPAM filtering
+          #
+          #   Name and summary matches only, use "search all" for everything.
+          # [root@centos7 ~]#
+
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
