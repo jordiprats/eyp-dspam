60c8ac1e2e407f484c9b59ab4e3c57bb5a5dd398
Jordi Prats <jprats@systemadmin.es>
2016-12-11 14:36:07 +0100

    nota dspam clamav

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,13 @@
 class dspam::config inherits dspam {
 
+  # Integrate ClamAV Into DSPAM
+  #
+  # /etc/dspam/dspam.conf
+  #
+  # Change the existing lines so that they look like this:
+  # ClamAVPort     3310
+  # ClamAVHost     127.0.0.1
+  # ClamAVResponse reject
+
+
 }
